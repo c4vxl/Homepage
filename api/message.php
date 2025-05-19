1690987d7a5f4a7c36f94ebefd19a93e5cf82504
c4vxl <manage@c4vxl.de>
2025-05-19 18:12:37 +0200

    added messages panel

--- api/message.php
+++ api/message.php
@@ -1,48 +1,6 @@
 <?php
 $output = array();
 
-if (!isset($_POST["mail"])) {
-    $output["error"] = "No mail!";
-    $output["errorID"] = 1;
-    $output["success"] = false;
-    die(json_encode($output));
-}
-
-if (!isset($_POST["name"])) {
-    $output["error"] = "No name!";
-    $output["errorID"] = 2;
-    $output["success"] = false;
-    die(json_encode($output));
-}
-
-if (!isset($_POST["msg"])) {
-    $output["error"] = "No message!";
-    $output["errorID"] = 3;
-    $output["success"] = false;
-    die(json_encode($output));
-}
-
-$mail = filter_var($_POST["mail"], FILTER_SANITIZE_EMAIL);
-$name = htmlspecialchars($_POST["name"], ENT_QUOTES, 'UTF-8');
-$message = htmlspecialchars($_POST["msg"], ENT_QUOTES, 'UTF-8');
-
-if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
-    $output["error"] = "Invalid email format!";
-    $output["errorID"] = 4;
-    $output["success"] = false;
-    die(json_encode($output));
-}
-
-$output["success"] = true;
-date_default_timezone_set('Europe/Paris');
-
-$entry = array(
-    "email"   => $mail,
-    "name"    => $name,
-    "message" => $message,
-    "time"    => date("Y-m-d H:i:s")
-);
-
 $db_path = "../messages/messages.json";
 $db_dir = pathinfo($db_path, PATHINFO_DIRNAME);
 
@@ -52,7 +10,66 @@ if (!is_file($db_path)) file_put_contents($db_path, json_encode([]));
 $content = json_decode(file_get_contents($db_path), true);
 if ($content === null) $content = [];
 
-array_push($content, $entry);
+
+if (isset($_GET["delete"])) {
+    $id = $_GET["delete"];
+    $entry = $content[$id];
+    if ($entry == null) {
+        $output["error"] = "No such message!";
+        $output["errorID"] = 5;
+        $output["success"] = false;
+    } else {
+        $output["success"] = true;
+        unset($content[$id]);
+        $content = array_values($content);
+    }
+}
+
+else {
+    if (!isset($_POST["mail"])) {
+        $output["error"] = "No mail!";
+        $output["errorID"] = 1;
+        $output["success"] = false;
+        die(json_encode($output));
+    }
+    
+    if (!isset($_POST["name"])) {
+        $output["error"] = "No name!";
+        $output["errorID"] = 2;
+        $output["success"] = false;
+        die(json_encode($output));
+    }
+    
+    if (!isset($_POST["msg"])) {
+        $output["error"] = "No message!";
+        $output["errorID"] = 3;
+        $output["success"] = false;
+        die(json_encode($output));
+    }
+    
+    $mail = filter_var($_POST["mail"], FILTER_SANITIZE_EMAIL);
+    $name = htmlspecialchars($_POST["name"], ENT_QUOTES, 'UTF-8');
+    $message = htmlspecialchars($_POST["msg"], ENT_QUOTES, 'UTF-8');
+    
+    if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        $output["error"] = "Invalid email format!";
+        $output["errorID"] = 4;
+        $output["success"] = false;
+        die(json_encode($output));
+    }
+    
+    $output["success"] = true;
+    date_default_timezone_set('Europe/Paris');
+    
+    $entry = array(
+        "email"   => $mail,
+        "name"    => $name,
+        "message" => $message,
+        "time"    => date("Y-m-d H:i:s")
+    );
+    
+    array_push($content, $entry);
+}
 
 file_put_contents($db_path, json_encode($content, JSON_PRETTY_PRINT));
 
---
+++ assets/script/messages.js
@@ -0,0 +1,54 @@
+let messages = [];
+
+const createMessage = (name, mail, date, content, id) => {
+    document.querySelector("#message__container").insertAdjacentHTML("afterbegin", `
+        <div class="message primary">
+            <p style="opacity: .7; text-align: center; margin-top: 0;">${date}</p>
+            <div class="row">
+                <span>Name:</span>
+                <input type="text" class="primary" disabled value="${name}">
+            </div>
+            <div class="row">
+                <span>E-Mail:</span>
+                <input type="text" class="primary" disabled value="${mail}">
+            </div>
+            <p>Message:</p>
+            <textarea class="primary" disabled>${content}</textarea>
+            <button class="primary" data-cursor-event="mail" onclick='reply(${id})'>Reply</button>
+            <button class="primary" data-cursor-event="none" onclick='deleteMessage(${id})'>Delete</button>
+        </div>`);
+}
+
+const reloadMessages = () => {
+    document.querySelector("#message__container").innerHTML = "";
+    fetch("messages/messages.json").then(response => response.json())
+        .then(response => {
+            messages = response;
+            if (messages.length == 0) {
+                document.querySelector("#message__container").innerHTML = "<p id='no__msgs'>It seems like there are no messages!</p>";
+            }
+            response.forEach((element, i) => {
+                let time = element.time.split(" ");
+                createMessage(element.name, element.email, time[0].split("-").reverse().join(".") + " - " + time[1], element.message, i) 
+            });
+        });
+}
+
+const reply = (id) => {
+    window.open(`mailto:${messages[id].email}`);
+}
+
+const deleteMessage = (id) => {
+    fetch(`api/message.php?delete=${id}`)
+        .then(x => x.json())
+        .then(x => {
+            if (x.success) {
+                showNotification("Success", "Message has been removed successfully!", "green");
+            } else {
+                showNotification("Error", x.error);
+            }
+        });
+    reloadMessages();
+}
+
+reloadMessages();
